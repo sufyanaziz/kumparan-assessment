f76fb88630b2a00b52ec72d520fefdd43120170e
Achmad Sufyan Aziz <49118546+sufyanaziz@users.noreply.github.com>
2021-10-17 12:48:43 +0700

    add Error Page

---
+++ src/pages/Error.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Layout from "../components/Layout";
+
+const ErrorContainer = styled.div`
+  height: 400px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  color: var(--dark);
+
+  h3 {
+    margin: 10px 0 5px 0;
+  }
+
+  h1,
+  a {
+    color: var(--mainColor);
+  }
+`;
+
+const Error = () => {
+  return (
+    <Layout>
+      <ErrorContainer>
+        <h1>404</h1>
+        <h3>Page is doesn't exist</h3>
+        <p>
+          Go to, <Link to="/">main page</Link>
+        </p>
+      </ErrorContainer>
+    </Layout>
+  );
+};
+
+export default Error;
