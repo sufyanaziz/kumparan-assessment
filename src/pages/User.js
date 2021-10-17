c28d9b7a55848390d066ec97a27b28428c902c1b
Achmad Sufyan Aziz <49118546+sufyanaziz@users.noreply.github.com>
2021-10-17 21:40:44 +0700

    update height body

--- src/pages/User.js
+++ src/pages/User.js
@@ -132,7 +132,7 @@ const UserPageContainer = styled.div`
           text-align: left;
           -webkit-line-clamp: 4;
           -webkit-box-orient: vertical;
-          height: 130px;
+          height: 133px;
           margin-bottom: 20px;
         }
 
